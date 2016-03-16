999ddce78f07caa170776828a139c290291b7393
Teo Stocco <teo.stocco@epfl.ch>
2016-03-16 14:21:22 +0100

    fix printout

--- done/week04/db_utils.c
+++ done/week04/db_utils.c
@@ -38,7 +38,7 @@ void print_header (const struct pictdb_header header){
     printf("DB NAME:%31s\n", header.db_name);
     printf("DB VERSION: %" PRIu32 "\n", header.db_version);
     printf("IMAGE COUNT: %" PRIu32 "\t\tMAX IMAGES: %" PRIu32 "\n", header.num_files, header.max_files);
-    printf("THUMBAIL: %" PRIu16 " x %" PRIu16 "\tSMALL: %" PRIu16 " x %" PRIu16 "\n",
+    printf("THUMBAIL: %" PRIu16 " x %" PRIu16 "\tSMALL %" PRIu16 " x %" PRIu16 "\n",
            header.res_resized[0], header.res_resized[1], header.res_resized[2], header.res_resized[3]);
     puts("***********DATABASE HEADER END***********");
     puts("*****************************************");
@@ -56,9 +56,9 @@ void print_metadata (const struct pict_metadata metadata)
     printf("SHA: %s\n", sha_printable);
     printf("VALID: %" PRIu16 "\n", metadata.is_valid);
     printf("UNUSED: %" PRIu16 "\n", metadata.unused_16);
-    printf("OFFSET ORIG. : %" PRIu64 "\t\tSIZE ORIG. : %" PRIu32 "\n", metadata.offset[RES_ORIG], metadata.size[RES_ORIG]);
-    printf("OFFSET THUMB. : %" PRIu64 "\t\tSIZE THUMB. : %" PRIu32 "\n", metadata.offset[RES_THUMB], metadata.size[RES_THUMB]);
-    printf("OFFSET SMALL : %" PRIu64 "\t\tSIZE SMALL : %" PRIu32 "\n", metadata.offset[RES_SMALL], metadata.size[RES_SMALL]);
-    printf("ORIGINAL : %" PRIu32 " x %" PRIu32 "\n", metadata.res_orig[0], metadata.res_orig[1]);
+    printf("OFFSET ORIG.: %" PRIu64 "\t\tSIZE ORIG.: %" PRIu32 "\n", metadata.offset[RES_ORIG], metadata.size[RES_ORIG]);
+    printf("OFFSET THUMB.: %" PRIu64 "\t\tSIZE THUMB.: %" PRIu32 "\n", metadata.offset[RES_THUMB], metadata.size[RES_THUMB]);
+    printf("OFFSET SMALL: %" PRIu64 "\t\tSIZE SMALL: %" PRIu32 "\n", metadata.offset[RES_SMALL], metadata.size[RES_SMALL]);
+    printf("ORIGINAL: %" PRIu32 " x %" PRIu32 "\n", metadata.res_orig[0], metadata.res_orig[1]);
     puts("****************************************");
 }
