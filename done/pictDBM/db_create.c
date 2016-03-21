345e7879c1152074b6d525637792e9bc48ebbd7b
asoccard <aurelien.soccard@epfl.ch>
2016-03-21 08:44:05 +0100

    Do_create function implemented

--- done/pictDBM/db_create.c
+++ done/pictDBM/db_create.c
@@ -11,9 +11,9 @@
 #include <string.h> // for strncpy
 
 /********************************************************************//**
- * Creates the database called db_filename. Writes the header and the
- * preallocated empty metadata array to database file.
- */
+                                                                       * Creates the database called db_filename. Writes the header and the
+                                                                       * preallocated empty metadata array to database file.
+                                                                       */
 /* **********************************************************************
  * TODO: ADD THE PROTOTYPE OF do_create HERE.
  * **********************************************************************
@@ -23,10 +23,45 @@ int db_create (const char* filename, struct pictdb_file db_file)
     // Sets the DB header name
     strncpy(db_file.header.db_name, CAT_TXT,  MAX_DB_NAME);
     db_file.header.db_name[MAX_DB_NAME] = '\0';
-
+    
     /* **********************************************************************
      * TODO: WRITE YOUR CODE HERE
      * **********************************************************************
      */
+    
+    if (filename == NULL) {
+        
+        return ERR_INVALID_FILENAME;
+        
+    } else {
+        
+        db_file.fpdb = fopen(filename, "wb");
+        
+        if (db_file.fpdb == NULL) {
+            
+            return ERR_IO;
+            
+        } else {
+            // Since now the file is open, we need to close it before returning anything
+            int errorCode = 0;
+            
+            if (1 != fwrite(&db_file.header , sizeof(struct pictdb_header), 1, db_file.fpdb)){
+                errorCode = ERR_IO;
+            }
+            if ( db_file.header.max_files != fwrite(db_file.metadata, sizeof(struct pict_metadata), db_file.header.max_files, db_file.fpdb)){
+                errorCode = ERR_IO;
+            }
+            
+            fclose(db_file.fpdb);
+            
+            // The stream is closed, we return now as we don't want to print anything in case of an error
+            if (errorCode != 0){
+                return errorCode;
+            }
+            
+            printf("%d item(s) written\n", 1+db_file.header.max_files);
+        }
+
+    }
     return 0;
 }
