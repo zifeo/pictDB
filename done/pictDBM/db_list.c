c7a0d9197b9e8b976bd021ed9429b2854e7e83fa
Teo Stocco <teo.stocco@epfl.ch>
2016-05-07 21:26:29 +0200

    terminate json

--- done/pictDBM/db_list.c
+++ done/pictDBM/db_list.c
@@ -6,7 +6,12 @@
  * @date 9 March 2016
  */
 
+#include <stdlib.h>
 #include "pictDB.h"
+#include <json-c/json.h>
+
+#define UNKNOWN_MODE "unimplemented do_list mode"
+#define PICS_JSON_LABEL "Pictures"
 
 /********************************************************************//**
  * List all pictures included in db_file.
@@ -18,29 +23,46 @@ const char* do_list(const struct pictdb_file* db_file, enum do_list_mode mode)
     }
 
     if (mode != STDOUT && mode != JSON) {
-        return "unimplemented do_list mode";
-    }
-
-    print_header(&db_file->header);
-
-    if (db_file->header.num_files > 0) {
-        for (size_t i = 0; i < db_file->header.max_files; ++i) {
-            if (db_file->metadata[i].is_valid == NON_EMPTY) {
-                print_metadata(&db_file->metadata[i]);
-            }
-        }
-    } else {
-        printf("<< empty database >>\n");
+        // TODO : what to do here ?
+        return UNKNOWN_MODE;
     }
 
     switch (mode) {
-        case STDOUT:
+    case STDOUT: {
 
-            break;
-        case JSON:
+        print_header(&db_file->header);
 
-            break;
+        if (db_file->header.num_files > 0) {
+            for (size_t i = 0; i < db_file->header.max_files; ++i) {
+                if (db_file->metadata[i].is_valid == NON_EMPTY) {
+                    print_metadata(&db_file->metadata[i]);
+                }
+            }
+        } else {
+            printf("<< empty database >>\n");
+        }
+
+        return NULL;
     }
+    case JSON: {
 
-    return NULL;
+        struct json_object *obj = json_object_new_object();
+        struct json_object *arr = json_object_new_array();
+
+        for (size_t i = 0; i < db_file->header.max_files; ++i) {
+            if (db_file->metadata[i].is_valid == NON_EMPTY) {
+
+                struct json_object* id = json_object_new_string(db_file->metadata[i].pict_id);
+                // TODO : return code ?
+                json_object_array_add(arr, id);
+            }
+        }
+
+        json_object_object_add(obj, PICS_JSON_LABEL, arr);
+        const char* json = json_object_to_json_string(obj);
+        json_object_put(obj);
+
+        return json;
+    }
+    }
 }
--- done/pictDBM/pictDB_server.c
+++ done/pictDBM/pictDB_server.c
@@ -9,44 +9,97 @@
  */
 
 #include "libmongoose/mongoose.h"
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
 #include "pictDB.h"
 
-#define PORT 8000
+#define PORT "8000"
 
+#define ROUTE_LIST "/pictDB/list"
 
-static const char *s_http_port = "8000";
+// TODO : better than global ?
+static struct pictdb_file myfile;
+static int s_sig_received = 0;
 static struct mg_serve_http_opts s_http_server_opts;
 
-static void ev_handler(struct mg_connection *nc, int ev, void *p) {
-    if (ev == MG_EV_HTTP_REQUEST) {
-        mg_serve_http(nc, (struct http_message *) p, s_http_server_opts);
+static void handle_list(struct mg_connection *nc, struct http_message *hm)
+{
+    const char* rep = do_list(&myfile, JSON);
+     // TODO : leak/error ?
+
+    // TODO : better routines ?
+    mg_printf(nc, "HTTP/1.0 200 OK\r\nContent-Length: %d\r\n"
+              "Content-Type: application/json\r\n\r\n%s",
+              (int) strlen(rep), rep);
+
+}
+
+static void signal_handler(int sig_num)
+{
+    signal(sig_num, signal_handler);
+    s_sig_received = sig_num;
+}
+
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
+{
+    struct http_message *hm = (struct http_message *) ev_data;
+
+    switch (ev) {
+    case MG_EV_HTTP_REQUEST:
+        if (mg_vcmp(&hm->uri, ROUTE_LIST) == 0) {
+            handle_list(nc, hm);
+        } else {
+            mg_serve_http(nc, hm, s_http_server_opts);
+        }
+        break;
+    default:
+        break;
     }
 }
 
 /********************************************************************//**
  * MAIN
  ********************************************************************** */
-int main(int argc, char *argv[]) {
-    struct mg_mgr mgr;
-    struct mg_connection *nc;
+int main(int argc, char *argv[])
+{
 
-    mg_mgr_init(&mgr, NULL);
-    nc = mg_bind(&mgr, s_http_port, ev_handler);
-
-    // Set up HTTP server parameters
-    mg_set_protocol_http_websocket(nc);
-    s_http_server_opts.document_root = ".";  // Serve current directory
-    s_http_server_opts.dav_document_root = ".";  // Allow access via WebDav
-    s_http_server_opts.enable_directory_listing = "yes";
-
-    printf("Starting web server on port %s\n", s_http_port);
-    for (;;) {
-        mg_mgr_poll(&mgr, 1000);
+    if (argc < 2) {
+        return ERR_NOT_ENOUGH_ARGUMENTS;
     }
-    mg_mgr_free(&mgr);
 
+    if (strlen(argv[1]) == 0 || strlen(argv[1]) > FILENAME_MAX) {
+        return ERR_INVALID_FILENAME;
+    }
+
+    const char *db_filename = argv[1];
+    int status = do_open(db_filename, "rb", &myfile);
+
+    if (status == 0) {
+        print_header(&myfile.header);
+
+        struct mg_mgr mgr;
+        struct mg_connection *nc;
+
+        // TODO : sigint ?
+        signal(SIGTERM, signal_handler);
+        signal(SIGINT, signal_handler);
+
+        mg_mgr_init(&mgr, NULL);
+        nc = mg_bind(&mgr, PORT, ev_handler);
+        if (nc == NULL) {
+            // TODO : mongoose which error
+            return ERR_IO;
+        }
+
+        mg_set_protocol_http_websocket(nc);
+        s_http_server_opts.document_root = ".";
+        s_http_server_opts.enable_directory_listing = "yes";
+
+        while (!s_sig_received) {
+            mg_mgr_poll(&mgr, 1000);
+        }
+
+        mg_mgr_free(&mgr);
+    }
+
+    do_close(&myfile);
     return 0;
 }
