c745c726222d0eefed993a1845e6486c7a13f4ce
Teo Stocco <teo.stocco@epfl.ch>
2016-07-31 13:30:42 +0200

    add errors

---
+++ pictDBM/error.h
@@ -0,0 +1,45 @@
+
+#include <stdlib.h>
+
+#ifndef PICTDBPRJ_ERROR_H
+#define PICTDBPRJ_ERROR_H
+
+/**
+ * @brief pictDB library internal error codes.
+ *
+ */
+enum error_codes {
+    ERR_IO = 1,
+    ERR_OUT_OF_MEMORY,
+    ERR_NOT_ENOUGH_ARGUMENTS,
+    ERR_INVALID_FILENAME,
+    ERR_INVALID_COMMAND,
+    ERR_INVALID_ARGUMENT,
+    ERR_MAX_FILES,
+    ERR_RESOLUTIONS,
+    ERR_INVALID_PICID,
+    ERR_FULL_DATABASE,
+    ERR_FILE_NOT_FOUND,
+    NOT_IMPLEMENTED,
+    ERR_DUPLICATE_ID,
+    ERR_VIPS,
+    ERR_DEBUG
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief pictDB library internal error messages.
+ *
+ */
+extern
+const char * const ERROR_MESSAGES[];
+
+const size_t ERROR_COUNT;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
